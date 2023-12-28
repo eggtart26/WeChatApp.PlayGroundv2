b0c12db7c37893e986f2eb0ebbc14ad2be0e40f0
eggtart26 <justin.guan26@gmail.com>
2023-12-27 22:30:33 -0500

    init setup

---
+++ src/main/java/com/example/wechatappplaygroundv2/controller/AssetController.java
@@ -0,0 +1,16 @@
+package com.example.wechatappplaygroundv2.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class AssetController {
+    @GetMapping("/asset/get")
+    public String getAssets() {
+        return "Hello, this is a Get request!";
+    }
+
+    @PostMapping("asset/onboard")
+    public String onboardAssets(@RequestBody String message) {
+        return "You posted: " + message;
+    }
+}
